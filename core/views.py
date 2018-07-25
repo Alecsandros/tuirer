18ac9c6fc237022af92a0a0222737a6064620cae
Alecsandro <ascj@cin.ufpe.br>
2018-07-25 17:08:50 -0300

    variáveis de contexto

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .helpers import get_character_name, ip_info
+from datetime import datetime
 
 def index(request):
-    country = ip_info('country_name')
-    flag = ip_info('flag')
-    flag_image = f'<img src="{flag}" width="120" />'
-    return render(request, 'index.html')
+    context = {
+        'now': datetime.now()
+    }
+    return render(request, 'index.html', context)
 
