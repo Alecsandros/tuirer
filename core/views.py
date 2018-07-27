446599015c58e27b87bcc88548754a12f21f7770
Alecsandro <ascj@cin.ufpe.br>
2018-07-27 17:27:08 -0300

    For com conteúdo dos tuites

--- core/views.py
+++ core/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .helpers import get_character_name, ip_info
 from datetime import datetime
+from tuites.models import Tuite
 
 def index(request):
     context = {
-        'now': datetime.now()
+        'now': datetime.now(),
+        'tuites': Tuite.objects.all(),
     }
     return render(request, 'home.html', context)
 
